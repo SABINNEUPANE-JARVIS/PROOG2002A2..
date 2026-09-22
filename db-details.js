dc5fd679fe0ee46e7c3abb17f508237263a6539d
SABINNEUPANE-JARVIS <sabinn432@gmail.com>
2026-09-22 23:13:35 +1000

    Add database login details config file

---
+++ db-details.js
@@ -0,0 +1,9 @@
+// database login details 
+// This file keeps private database information seperate from the connection code.
+// so we can easily change the database login details without having to change the connection code.
+Module.exports = {
+    host: "localhost",
+    user: "root",
+    password: "Motorcycle@9817",
+    database: "charityevents_db"
+};
