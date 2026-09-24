bae4f7c75047b10824256de72d680ee27cf30460
SABINNEUPANE-JARVIS <sabinn432@gmail.com>
2026-09-24 14:45:32 +1000

    Update db-details.js

--- db-details.js
+++ db-details.js
@@ -1,7 +1,7 @@
-// database login details 
+// Database login details 
 // This file keeps private database information seperate from the connection code.
 // so we can easily change the database login details without having to change the connection code.
-Module.exports = {
+module.exports = {
     host: "localhost",
     user: "root",
     password: "Motorcycle@9817",
